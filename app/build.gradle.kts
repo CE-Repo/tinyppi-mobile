47dfa59e61e70d0f9521ec3fdbd0283b96c018cd
U3knOwn <jamal.2362@googlemail.com>
2026-09-23 14:26:46 +0200

    Bump version to 1.4.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.jamal2367.tinyppimobile"
         minSdk = 26
         targetSdk = 37
-        versionCode = 148
-        versionName = "1.4.8"
+        versionCode = 149
+        versionName = "1.4.9"
 
         val buildNumber = providers.environmentVariable("BUILD_NUMBER").orNull
             ?.trim()
