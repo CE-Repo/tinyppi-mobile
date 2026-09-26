bf65213fadabbaa305ac4cf1040b861b133c2419
U3knOwn <jamal.2362@googlemail.com>
2026-09-26 11:42:03 +0200

    Bump version to 1.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.jamal2367.tinyppimobile"
         minSdk = 26
         targetSdk = 37
-        versionCode = 152
-        versionName = "1.5.2"
+        versionCode = 153
+        versionName = "1.5.3"
 
         val buildNumber = providers.environmentVariable("BUILD_NUMBER").orNull
             ?.trim()
